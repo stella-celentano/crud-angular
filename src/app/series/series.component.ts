d64604fd0d9919167b3ad7524091328339cac352
stella-celentano <stellacelentano99@gmail.com>
2020-02-16 19:26:23 -0300

    Gerado Service Series

--- src/app/series/series.component.ts
+++ src/app/series/series.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Series } from './series.model'
 
 @Component({
   selector: 'app-series',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeriesComponent implements OnInit {
 
-  constructor() { }
+  public serie: Series[]
+
+  constructor( ) { }
 
   ngOnInit() {
   }
---
+++ src/app/series/series.model.ts
@@ -0,0 +1,5 @@
+export interface Series {
+    name: String,
+    abstract: String,
+    release: Date
+}
